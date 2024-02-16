700ae8635a4755277d8946031ccf4c8ee826900a
Kooreis <kooreis.official@gmail.com>
2024-02-16 07:19:13 -0600

    This update adds the areIdentical function, which checks if two binary trees are identical. It does this by recursively comparing the data in the nodes and their children.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,11 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+function areIdentical(root1, root2) {
+    if (root1 == null && root2 == null) {
+        return true;
     }
+    if (root1 == null || root2 == null) {
+        return false;
+    }
+    return (root1.data == root2.data &&
+        areIdentical(root1.left, root2.left) &&
+        areIdentical(root1.right, root2.right));
 }
